9bf8521e40dabbfeb82731529b59cf03093c3ac9
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-17 14:41:53 -0400

    added picture component and installed axios

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../src/App.css";
+import Header from "./Header";
+import Picture from "./Picture";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Picture />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Picture.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Picture = () => {
+  return <div>Picture goes here</div>;
+};
+
+export default Picture;
